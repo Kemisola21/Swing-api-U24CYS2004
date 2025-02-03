74c7db6c63bc2707417fcc527235c3b4fec1caf4
Kemisola21 <kemi29204@gmail.com>
2025-02-03 05:42:18 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package New;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.print("this is Csc 201");
+        System.out.print("this is a practical class");
+    }}
+    
+
+
